742c27f2b5707125ae5c4a21d89240b4e334e91a
Ehallett2 <146337550+Ehallett2@users.noreply.github.com>
2023-10-05 12:52:27 +0100

    Updating Class

--- Tasks/Task-103-HelloWorld/main.cpp
+++ Tasks/Task-103-HelloWorld/main.cpp
@@ -29,7 +29,7 @@ int main()
     // Write to LCD using the LCD_16X2_DISPLAY driver class
     lcd.printf("Hello World");
     lcd.locate(1, 0);   //Row 1, Col 0
-    lcd.printf("ELEC143");
+    lcd.printf("PROJ100");
 
     // This is a variable (not an oject as it has no functions) that stores a whole number (integer) in memory
     // (I used the keyword volatile to force it to use memory... long story and one for later)
