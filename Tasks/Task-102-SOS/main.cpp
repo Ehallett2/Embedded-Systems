a1e0bf9b5b28ff67afdf73a2993db797821a168f
Ehallett2 <146337550+Ehallett2@users.noreply.github.com>
2023-10-05 12:52:02 +0100

    Adding Scale

--- Tasks/Task-102-SOS/main.cpp
+++ Tasks/Task-102-SOS/main.cpp
@@ -2,7 +2,8 @@
 #include "uop_msb.h"
 using namespace uop_msb;
 
-#define WAIT_TIME_MS 500 
+#define WAIT_TIME_MS 1000 
+
 DigitalOut greenLED(TRAF_GRN1_PIN);
 Buzzer buzz;
 Buttons buttons;
@@ -20,17 +21,28 @@ int main()
     //Repeat everything "forever" (until the power is removed or the chip is reset)
     while (true)
     {
-        //On for 500ms
+        //On for 250ms each note in scale with light remaining on.
         greenLED = 1;
         buzz.playTone("C");
-        wait_us(WAIT_TIME_MS * 1000);  //500ms
-
-
-        //Off for 500ms
-        greenLED = 0;
+        wait_us(WAIT_TIME_MS * 250);  //250ms
+        buzz.playTone("D");
+        wait_us(WAIT_TIME_MS * 250);  //250ms
+        buzz.playTone("E");
+        wait_us(WAIT_TIME_MS * 250);  //250ms
+        buzz.playTone("F");
+        wait_us(WAIT_TIME_MS * 250);  //250ms
+        buzz.playTone("G");
+        wait_us(WAIT_TIME_MS * 250);  //250ms
+        buzz.playTone("A", Buzzer::HIGHER_OCTAVE);
+        wait_us(WAIT_TIME_MS * 250);  //250ms
+        buzz.playTone("B", Buzzer::HIGHER_OCTAVE);
+        wait_us(WAIT_TIME_MS * 250);  //250ms
         buzz.playTone("C", Buzzer::HIGHER_OCTAVE);
-        wait_us(WAIT_TIME_MS * 1000);  //500ms
+        wait_us(WAIT_TIME_MS * 250);  //250ms
 
+        //Off for 250ms
+        greenLED = 0;
+        
         //Pause
         buzz.rest();
         wait_us(WAIT_TIME_MS * 1000);
