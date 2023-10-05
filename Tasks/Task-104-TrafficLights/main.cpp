a36c1c931979ee546f774d930756e3e18535cfd9
Ehallett2 <146337550+Ehallett2@users.noreply.github.com>
2023-10-05 15:47:15 +0100

    Hello World Ex

--- Tasks/Task-104-TrafficLights/main.cpp
+++ Tasks/Task-104-TrafficLights/main.cpp
@@ -1,9 +1,11 @@
 #include "uop_msb.h"
+#include <algorithm>
+#include <type_traits>
 using namespace uop_msb;
 
 #define WAIT_TIME_MS 500 
 
-DigitalOut red(TRAF_RED1_PIN,1);         //Note the initial state
+DigitalOut red(TRAF_RED1_PIN,0);         //Note the initial state
 DigitalOut amber(TRAF_YEL1_PIN,0);
 DigitalOut green(TRAF_GRN1_PIN,0);
 
@@ -12,27 +14,46 @@ LCD_16X2_DISPLAY lcd;
 
 int main()
 {
-    lcd.puts("RED");
-    wait_us(1000000);
-
-    amber = 1;
-    lcd.cls();
-    lcd.puts("Amber");
-    wait_us(1000000);
-
-    green = 1;
-    lcd.cls();
-    lcd.puts("Green");    
-    wait_us(1000000);
-
-    lcd.cls();
-    lcd.puts("TASK-104");
-
     while (true)
-    {
-        red = !red;
-        amber = !amber;
-        green = !green;
-        wait_us(WAIT_TIME_MS * 1000);
+    {   
+        red = 1;
+        lcd.cls();
+        lcd.puts("RED");
+        wait_us(10000000);
+
+        amber = 1;
+        lcd.cls();
+        lcd.puts("RED + AMBER");
+        wait_us(2000000);
+        red = 0;
+        amber = 0;
+
+        green = 1;
+        lcd.cls();
+        lcd.puts("GREEN");    
+        wait_us(10000000);
+        green = 0;
+
+        lcd.cls();
+        lcd.puts("FLASH AMBER");
+        int count = 0;
+        while (count < 8)
+        {
+            amber = !amber;
+            wait_us(500000);
+            count = count + 1;
+        }
+        
+        lcd.cls();
+        lcd.puts("TASK-104");
+        wait_us(500000);
+        
+        /*while (true)
+        {
+            red = !red;
+            amber = !amber;
+            green = !green;
+            wait_us(WAIT_TIME_MS * 1000);
+        }*/
     }
 }
